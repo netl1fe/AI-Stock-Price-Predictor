69da0bed816e96dda409dee16be1f81c53d78082
net1ife <127271713+net1ife@users.noreply.github.com>
2023-07-18 18:46:32 -0500

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import time
 import torch
+import yfinance as yf
 from datetime import datetime, timedelta
 from sklearn.preprocessing import MinMaxScaler
 from helpers import fetch_historical_data, add_selected_ta_features, reorder_data, LSTMModel, create_sequences
@@ -18,10 +19,19 @@ def typing_effect(text, speed=0.03):
         placeholder.text(line) 
         st.write("")
 
+def fetch_single_day_data(ticker, date):
+    data = yf.download(ticker, start=date, end=date + timedelta(days=1))
+    return data
+
+# Function to calculate accuracy
+def calculate_accuracy(actual, predicted):
+    return abs((actual - predicted) / actual) * 100
+
+
 # Sidebar inputs
 ticker = st.sidebar.text_input("Ticker", "SPY")
-start_date = st.sidebar.date_input("Start Date", datetime.now() - timedelta(days=365 * 5))
-end_date = st.sidebar.date_input("End Date", datetime.now() - timedelta(days=1))
+start_date = datetime(1993, 1, 30)
+end_date = st.sidebar.date_input("End Date", datetime.now())  
 seq_length = 60  # Sequence length is now fixed to 60
 
 # Fetching and processing the data
@@ -36,12 +46,12 @@ scaler = MinMaxScaler()
 normalized_data = scaler.fit_transform(data.values)
 
 # Load the best model
-best_model = LSTMModel(data.shape[1], 42, 1, data.shape[1]).to(device)
-best_model.load_state_dict(torch.load('bmw.pth'))
+best_model = LSTMModel(data.shape[1], 94, 1, data.shape[1]).to(device)
+best_model.load_state_dict(torch.load('bmw2.pth'))
 best_model.eval()
 
 # Get the last sequence of data
-last_sequence = torch.from_numpy(normalized_data[-seq_length:-1].reshape(1, -1, data.shape[1])).float().to(device)  # Exclude today's data from the sequence
+last_sequence = torch.from_numpy(normalized_data[-seq_length-1:-1].reshape(1, -1, data.shape[1])).float().to(device)  # Exclude the last day's data from the sequence
 
 # Make a prediction
 predicted_value = best_model(last_sequence)
@@ -50,8 +60,20 @@ predicted_value = predicted_value.cpu().detach().numpy()
 # Rescale the predicted value
 predicted_value = scaler.inverse_transform(predicted_value)
 
-# Fetch the actual values for the day of prediction
-actual_values = data.values[-1]  # Get today's actual values
+# Fetch the actual values for the end_date
+end_date_data = fetch_single_day_data(ticker, end_date)
+
+# Ensure end_date_data is not empty and has valid values, otherwise handle the exception
+if end_date_data.empty:
+    st.write(f"No data available for {end_date.strftime('%Y-%m-%d')}.")
+else:
+    end_date_values = end_date_data.values[0]  # assuming OHLCV structure
+
+# Calculate the accuracy for each prediction
+accuracy_open = calculate_accuracy(end_date_values[0], predicted_value[0][0])
+accuracy_high = calculate_accuracy(end_date_values[1], predicted_value[0][1])
+accuracy_low = calculate_accuracy(end_date_values[2], predicted_value[0][2])
+accuracy_close = calculate_accuracy(end_date_values[3], predicted_value[0][3])
 
 # Display the predicted values with typing effect
 st.markdown("### Predicted Values")
@@ -63,12 +85,19 @@ Close: {predicted_value[0][3]:.2f}
 """
 typing_effect(output_text, speed=0.03)
 
-# Display the actual values for the day of prediction with typing effect
+# Display the actual values for the end_date with typing effect
 st.markdown("### Actual Values")
 output_text = f"""
-Open: {actual_values[0]:.2f}
-High: {actual_values[1]:.2f}
-Low: {actual_values[2]:.2f}
-Close: {actual_values[3]:.2f}
+Open: {end_date_values[0]:.2f}
+High: {end_date_values[1]:.2f}
+Low: {end_date_values[2]:.2f}
+Close: {end_date_values[3]:.2f}
 """
 typing_effect(output_text, speed=0.03)
+
+# Display the accuracy scores on the right side
+st.sidebar.markdown("### Accuracy Scores")
+st.sidebar.write(f"Open: {accuracy_open:.2%}")
+st.sidebar.write(f"High: {accuracy_high:.2%}")
+st.sidebar.write(f"Low: {accuracy_low:.2%}")
+st.sidebar.write(f"Close: {accuracy_close:.2%}")
