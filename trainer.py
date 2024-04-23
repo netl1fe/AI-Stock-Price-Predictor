0e5aaf680fd153d6cde912f244cf33cb140d2e63
wjb000 <127271713+wjb000@users.noreply.github.com>
2024-04-22 21:07:08 -0500

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -11,13 +11,11 @@ from helpers import fetch_historical_data, add_selected_ta_features, reorder_dat
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# Sidebar inputs
 ticker = st.sidebar.text_input("Ticker", "SPY")
 start_date = datetime(1993, 1, 30)
 end_date = st.sidebar.date_input("End Date", datetime.now() - timedelta(days=1))
 seq_length = st.sidebar.slider("Sequence Length", min_value=1, max_value=200, value=60)
 
-# Fetching and processing the data
 @st.cache_data
 def get_processed_data(ticker, start_date, end_date):
     data = fetch_historical_data(ticker, start_date, end_date)
@@ -25,16 +23,13 @@ def get_processed_data(ticker, start_date, end_date):
         return None, None, None
     data = add_selected_ta_features(data)
     data = reorder_data(data.dropna())
-    # Split data into training and validation sets before normalizing
     train_data, valid_data = train_test_split(data, test_size=0.2, shuffle=False)
-    # Normalize data
     train_data, train_scaler, original_train_data = normalize_data(train_data.values)
     valid_data, _, original_valid_data = normalize_data(valid_data.values)
     return train_data, valid_data, original_train_data, original_valid_data, train_scaler
 
 train_data, valid_data, original_train_data, original_valid_data, train_scaler = get_processed_data(ticker, start_date, end_date)
 
-# Create sequences
 X_train, y_train = create_sequences(train_data, seq_length)
 X_valid, y_valid = create_sequences(valid_data, seq_length)
 
@@ -45,10 +40,8 @@ y_valid = torch.from_numpy(y_valid).float().to(device)
 
 input_dim = train_data.shape[1]
 
-# Global variables for saving the best model
 best_valid_loss = float('inf')
 
-# Define the function we want to optimize
 def optimize_model(hidden_dim, num_layers, learning_rate):
     global best_valid_loss
     hidden_dim = int(hidden_dim)
@@ -70,9 +63,8 @@ def optimize_model(hidden_dim, num_layers, learning_rate):
         }
         with open('best_params.json', 'w') as f:
             json.dump(best_params, f)
-    return -valid_loss  # We want to maximize the negative loss (i.e., minimize the loss)
+    return -valid_loss
 
-# Define the bounds of the parameters we want to optimize
 pbounds = {
     'hidden_dim': (1, 100),
     'num_layers': (1, 5),
@@ -86,10 +78,8 @@ optimizer = BayesianOptimization(
     random_state=1,
 )
 
-# Run the optimization
 optimizer.maximize(init_points=200, n_iter=1000)
 
-# Load the best parameters
 with open('best_params.json', 'r') as f:
     best_params = json.load(f)
 
@@ -107,28 +97,27 @@ Number of Layers: {best_params['num_layers']}
 Learning Rate: {best_params['learning_rate']}
 """
 
-typing_speed = 0.03  # Adjust the typing speed (in seconds) to control the effect
+typing_speed = 0.03
 
-# Split the output into lines
+
 lines = output_text.strip().split("\n")
 
-# Simulate typing effect for each line
+
 for line in lines:
     placeholder = st.empty()
     for char in line:
         placeholder.text(char)
         time.sleep(typing_speed)
-    placeholder.text(line)  # Display the full line after typing
-    st.write("")  # Add an empty line after each output line
+    placeholder.text(line)
+    st.write("")
 
-# Train the model with the best parameters
 best_model = LSTMModel(input_dim, best_params['hidden_dim'], best_params['num_layers'], input_dim).to(device)
 criterion = torch.nn.MSELoss(reduction='mean').to(device)
 optimizer = torch.optim.Adam(best_model.parameters(), lr=best_params['learning_rate'])
 best_model, _ = train_model(best_model, X_train, y_train, 100, criterion, optimizer)
-torch.save(best_model.state_dict(), 'best_model_weights.pth')  # Save the best model
+torch.save(best_model.state_dict(), 'best_model_weights.pth')
 
-# Show the loss on the validation set with typing effect
+
 st.markdown("### Validation Loss")
 valid_loss = model_loss(best_model, X_valid, y_valid, criterion)
 
@@ -144,22 +133,17 @@ for line in lines:
     placeholder.text(line)
     st.write("")
 
-# Load the best model
 best_model = LSTMModel(input_dim, best_params['hidden_dim'], best_params['num_layers'], input_dim).to(device)
 best_model.load_state_dict(torch.load('best_model_weights.pth'))
 best_model.eval()
 
-# Get the last sequence of data
 last_sequence = torch.from_numpy(valid_data[-seq_length:].reshape(1, -1, input_dim)).float().to(device)
 
-# Make a prediction
 predicted_value = best_model(last_sequence)
 predicted_value = predicted_value.cpu().detach().numpy()
 
-# Rescale the predicted value
 predicted_value = train_scaler.inverse_transform(predicted_value)
 
-# Fetch the actual values
 actual_values = original_valid_data[-1]
 
 st.markdown("### Actual Values")
